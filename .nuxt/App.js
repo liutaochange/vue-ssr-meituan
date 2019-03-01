ceb93760262a55e99d62880c2f146520a257dedf
liutaochange <liutaochange@163.com>
2019-03-01 18:32:50 +0800

    fix:

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import NuxtLoading from './components/nuxt-loading.vue'
 
+import '..\\node_modules\\element-ui\\lib\\theme-chalk\\reset.css'
+
 import '..\\node_modules\\element-ui\\lib\\theme-chalk\\index.css'
+
+import '..\\assets\\css\\main.css'
 
 import _6f6c098b from '..\\layouts\\default.vue'
 
