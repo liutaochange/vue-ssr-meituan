4c4aae0c75961738e21b70de0eb9728f8cfb3f88
liutaochange <liutaochange@163.com>
2019-03-01 21:39:39 +0800

    fix

---
+++ components/common/header/geo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/>北京
+    <!-- {{ $store.state.geo.position.city }} -->
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity">切换城市</nuxt-link>
+    [香河 廊坊 天津 固安县]
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
